2a8a6e1b02c3df8c9c5c64ecb035942862cc27e6
Diego Eis <diegoeis@gmail.com>
2017-10-09 10:04:30 -0300

    teste de cors.io

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -4,7 +4,7 @@ function(){
 
   selic.api = {}
 
-  const apiSelic = "http://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados?formato=json&dataInicial=01/01/2015&dataFinal=31/12/2020"
+  const apiSelic = "https://cors.io/?http://api.bcb.gov.br/dados/serie/bcdata.sgs.4390/dados?formato=json&dataInicial=01/01/2015&dataFinal=31/12/2020"
 
 
   function init() {
