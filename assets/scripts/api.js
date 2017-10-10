8f59d5b1f43559c8edafe4be45cb08a4e1cbc0b1
Diego Eis <diego@tableless.com.br>
2017-10-09 22:24:20 -0300

    voltando com o cors.io

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -11,31 +11,11 @@ function(){
   }
 
   selic.api.handleApi = () => {
-    // axios.get(apiSelic)
-    // .then(response => {
-    //   selic.info.init(response.data)
-    // })
-    // .catch(response => console.error(response))
-
-    $.ajax({
-        type: 'GET',
-      
-        url: apiSelic,
-      
-        contentType: 'text/plain',
-      
-        xhrFields: {
-          withCredentials: false
-        },
-      
-        success: function(data) {
-            selic.info.init(data)
-        },
-      
-        error: function() {
-          console.error(data)
-        }
-      });
+    axios.get(apiSelic)
+    .then(response => {
+      selic.info.init(response.data)
+    })
+    .catch(response => console.error(response))
   }
 
 
